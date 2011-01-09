0ec49cd84eff958d4a2ec7c01ee2d3a3a0ecc2a9
jaffa <jaffa@dfe33c29-9031-4868-a6bb-c0692212f013>
2011-01-09 00:19:46 +0000

    Modified test doc and executable for Windows compatibility
    git-svn-id: file:///home/finnerty/Documents/Programming/svnroot/butter@163 dfe33c29-9031-4868-a6bb-c0692212f013

--- trunk/test_butter/startit.bat
+++ trunk/test_butter/startit.bat
@@ -3,5 +3,5 @@ REM run test_suite.py
 REM
 set _time_=%TIME:.=%
 set _timestamp_=%DATE:~4,2%%_time_:~0,2%%_time_:~3,2%%_time_:~6,4%
-start /b test_suite.py --file test.xml %* > "out%_timestamp_: =0%.html"
+start /B /WAIT test_suite.py --file test.xml %* > "out%_timestamp_: =0%.html"
 
