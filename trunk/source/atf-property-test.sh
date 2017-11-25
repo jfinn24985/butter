64b0f3311e4a24ed8f17b070bd347dad0ac43c11
Justin Finnerty <jfinn24985@gmail.com>
2017-11-25 15:13:07 +0100

    Info in item description (FLAGS=) and jam generator.

--- trunk/source/atf-property-test.sh
+++ trunk/source/atf-property-test.sh
@@ -84,6 +84,69 @@ property_version_jam_gen_body() {
 }
 
 
+atf_test_case property_top_description_jam_gen
+property_top_description_jam_gen_head() {
+  atf_set "descr" "Description (*top*)Package (FLAGS=..) with jam generator."
+}
+property_top_description_jam_gen_body() {
+  pushd ../test/property_test
+  #----------------------- 
+  # PROJECT.description : FLAGS=... \ HDR=... \ LINK=...
+  # Standard (jam) variant 
+  #----------------------- 
+  setup_example "property_test" "jam" "top_description"
+  run_plugouts "property_test" "jam" "top_description"
+
+  build_test(){
+    local variant=$1
+    local builddir=$2
+    pushd output
+    # test base build target
+    atf_check -s exit:0 -o save:jam1.log -e save:jam1.err jam ${variant}
+    atf_check -s exit:0 [ -x ${builddir}/src/Executable/program ]
+    atf_check -s exit:0 [ -e ${builddir}/src/Library/library.a ]
+    atf_check -s exit:0 -o save:../output_default.canon ${builddir}/src/Executable/program
+    pushd ${builddir}/src/Executable
+      atf_check -s exit:0 -o save:symbol.txt objdump -t program
+      atf_check -s exit:1 -o ignore diff symbol.txt ../../../../symbol.canon
+    popd
+    # test install target
+    atf_check -s exit:0 -o save:jam2.log -e save:jam2.err jam install ${variant}
+    atf_check -s exit:0 [ -x installdir/bin/program ]
+    atf_check -s exit:0 [ -e installdir/lib/library.a ]
+    atf_check -s exit:0 -o file:../output_default.canon installdir/bin/program
+    # no distclean target
+    # test clean target
+    atf_check -s exit:0 -o save:jam3.log -e save:jam3.err jam clean ${variant}
+    atf_check -s exit:0 [ ! -e ${builddir}/src/Executable/program ]
+    atf_check -s exit:0 [ ! -e ${builddir}/src/Library/library.a ]
+    atf_check -s exit:0 [ -e installdir/bin/program ]
+    atf_check -s exit:0 [ -e installdir/lib/library.a ]
+
+    atf_check -s exit:0 -o empty rm jam1.log jam1.err
+    atf_check -s exit:0 -o empty rm jam2.log jam2.err
+    atf_check -s exit:0 -o empty rm jam3.log jam3.err
+    atf_check -s exit:0 -o empty rm -rf ${builddir}
+    atf_check -s exit:0 -o empty rm -rf installdir
+    popd
+  }
+
+  # default (DEBUG) VARIANT
+  build_test "" DEBUG
+  # specific DEBUG VARIANT
+  build_test -sVARIANT=DEBUG DEBUG
+  # RELEASE VARIANT
+  build_test -sVARIANT=RELEASE RELEASE
+
+  # Clean up
+  atf_check -s exit:0 -o empty rm -rf output
+  atf_check -s exit:0 -o empty git checkout HEAD -- .
+  atf_check -s exit:0 -o inline:"# On branch master\nnothing to commit, working directory clean\n" git status .
+  popd
+  unset build_test
+}
+
+
 atf_test_case property_top_ldflags_jam_gen
 property_top_ldflags_jam_gen_head() {
   atf_set "descr" "Property (*top*)Package(butter ldflags) with jam generator."
@@ -1124,6 +1187,7 @@ atf_init_test_cases() {
     atf_add_test_case property_top_flags_jam_gen
     atf_add_test_case property_top_ldflags_jam_gen
     atf_add_test_case property_top_include_jam_gen
+    atf_add_test_case property_top_description_jam_gen
     atf_add_test_case property_log_level_jam_gen
     atf_add_test_case property_libtype_static_jam_gen
     atf_add_test_case property_libtype_shared_jam_gen
